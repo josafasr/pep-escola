f1af77a0ec8c959bedb81648edef74f8e094c902
Marcos Antonio <mosther_marcos007@hotmail.com>
2020-05-05 11:31:38 -0300

    Atualição de update de agendamento

--- server/src/resolvers/agendamento.js
+++ server/src/resolvers/agendamento.js
@@ -11,12 +11,12 @@ export default {
   Query: {
 
     /**
-     * retorna todos os registros de recordatorio alimentar
+     * retorna todos os registros de agendamento
      */
     agendamentos: (parent, args, { models }) => models.Agendamento.findAll(),
 
     /**
-     * retorna um registro de recordatorio alimentar pelo id
+     * retorna um registro de agendamento pelo id
      */
     agendamento: (parent, { id }, { models }) => models.Agendamento.findByPk(id)
   },
@@ -57,16 +57,23 @@ export default {
      */
     updateAgendamento: async (parent, args, { models }) => {
       const result = await models.Agendamento.update({
-        nome: args.nome,
-        tipoRefeicaoId: args.tipoRefeicaoId,
+        insercao: args.insercao,
+        retorno: args.retorno,
+        ambulatorio: args.ambulatorio,
+        data_horario: args.data_horario,
+        confirmado: args.confirmado,
+        status: args.status,
+        cancelado: args.cancelado,
+        pacienteId: args.pacienteId,
+        usuarioId: args.usuarioId,
         updatedAt: new Date(),
       }, {
         where: { id: args.id },
         returning: true,
         plain: true
       })
-      const RecordatorioAlimentar = result[1]
-      return RecordatorioAlimentar
+      const agendamento = result[1]
+      return agendamento
     },
 
     /**
